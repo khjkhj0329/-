c5868a3dbbba50a45b0766ec2bafeaeeec1bb988
효정이 <w2029@e-mirim.hs.kr>
2021-11-24 23:33:54 +0900

    일반쓰레기, 재활용 이미지 넣기

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import msvcrt
 from idlelib import window
 from tkinter import *
+import tkinter as tk
 # class Join:
 #     def __init__(self, id):
 #         # 아이디
@@ -31,69 +32,74 @@ from tkinter import *
 class All():
     def __init__(self):
         self.start()
+        self.menu()
+
 
     def start(self):
-        self.start = Tk()
+        self.start = tk.Tk()
         self.start.title("나혼자 분리수거")
         self.start.geometry("1000x700+250+35")   # 창 크기와 창이 뜨는 위치
         self.start.resizable(False, False)   #창 크기 고정
         self.start.configure(bg='#708467')
-    #
-    #     img = PhotoImage(file='title.png')
-    #     img_title = Label(image=img, bg="#708467")
-    #     img_title.image = img
-    #     img_title.place(x=450, y=130)
-    #
-    #     title1 = Label(self.start, text="나혼자", bg="#708467")
-    #     title1.configure(font=('Corbel', 23))
-    #     title1.pack()
-    #     title1.place(x=450, y=233)
-    #     title2 = Label(self.start, text="분리수거", bg="#708467")
-    #     title2.configure(font=('Corbel', 23))
-    #     title2.pack()
-    #     title2.place(x=434, y=270)
-    #
-    #     id_txt = Entry(self.start, width=18, font=("궁서체", 20))
-    #     id_txt.pack()
-    #     id_txt.place(x=370, y=345, height=40)
-    #     pwd_txt = Entry(self.start, width=18, font=("궁서체", 20))
-    #     pwd_txt.pack()
-    #     pwd_txt.place(x=370, y=415, height=40)
-    #
-    #
-    #     login_btn = Button(self.start, bg="#F9F3D5", text="Login", pady=10, padx=90)
-    #     login_btn.pack()
-    #     login_btn.configure(font=("Courier", 15, "italic", "bold"))
-    #     login_btn.place(x=372, y=490)
-    #     self.start.mainloop()
+
+        # img = tk.PhotoImage(file='title.png')
+        # img_title = tk.Label(image=img, bg="#708467")
+        # img_title.image = img
+        # img_title.place(x=450, y=130)
+        #
+        # title1 = tk.Label(self.start, text="나혼자", bg="#708467")
+        # title1.configure(font=('Corbel', 23))
+        # title1.pack()
+        # title1.place(x=450, y=233)
+        # title2 = tk.Label(self.start, text="분리수거", bg="#708467")
+        # title2.configure(font=('Corbel', 23))
+        # title2.pack()
+        # title2.place(x=434, y=270)
+        #
+        # id_txt = tk.Entry(self.start, width=18, font=("궁서체", 20))
+        # id_txt.pack()
+        # id_txt.place(x=370, y=345, height=40)
+        # pwd_txt = tk.Entry(self.start, width=18, font=("궁서체", 20))
+        # pwd_txt.pack()
+        # pwd_txt.place(x=370, y=415, height=40)
+        #
+        #
+        # login_btn = tk.Button(self.start, bg="#F9F3D5", text="Login", pady=10, padx=90)
+        # login_btn.pack()
+        # login_btn.configure(font=("Courier", 15, "italic", "bold"))
+        # login_btn.place(x=372, y=490)
+        # self.start.mainloop()
 
 
     def menu(self):
-        self.menu_in = Tk()
+        self.menu_in = tk.Tk()
         self.menu_in.title("나혼자 분리수거")
         self.menu_in.geometry("1000x700+250+35")   # 창 크기와 창이 뜨는 위치
         self.menu_in.resizable(False, False)   #창 크기 고정
         self.menu_in.configure(bg='#708467')
+
+        title_img = tk.PhotoImage(file='title.png')
+        title_image = tk.Label(image=title_img, bg="#708467")
+        title_image.image = title_img
+        title_image.place(x=0, y=250)
+
+        img1 = tk.PhotoImage(file='trash1.png')
+        trash1 = tk.Button(image=img1, bg="#708467")
+        trash1.place(x=300, y=260)
+
+        img2 = tk.PhotoImage(file='trash2.png')
+        trash2 = tk.Button(image=img2, bg="#708467")
+        trash2.place(x=550, y=260)
         self.menu_in.mainloop()
 
-        img1 = PhotoImage(file='trash1.png')
-        trash1 = Label(image=img1)
-        trash1.image = img1
-        trash1.place(x=450, y=130)
-
-
-
-
-# lab1 = Label(Tk, text="ID")
-# lab1.grid(row=0, column=0)
-# txt = Entry(Tk)
-# txt.grid(row=0, column=1)
-# btn = Button(Tk, text="Login", width=200)
-# btn.grid(row=1, column=1)
+    def details_menu(self):
+        pass
 
 
 if __name__ == '__main__':
-    all = All()
+    all_in = All()
+
+
 
 
 
