30f7291156e0f9fad8d61cf530e67cde8c638ee9
효정이 <w2029@e-mirim.hs.kr>
2021-11-29 00:21:42 +0900

    싹 다 갈고 만들었다.. 매우 힘들었다...

--- details_menu.py
+++
@@ -1,6 +0,0 @@
-import tkinter as tk
-import main_in
-
-class Details_menu:
-    def __init__(self, details):
-        self.details = details
---
+++ mypage.py
@@ -0,0 +1,6 @@
+import tkinter
+
+class MyPage:
+    def __init__(self, my_info):
+        self.my_info = my_info
+
